b66b4a0cb7f1529f5091e977fd36d978215dab1c
Your Name <you@example.com>
2021-06-13 13:19:01 +0200

    Added new method stop engine

---
+++ src/Car.java
@@ -0,0 +1,31 @@
+public class Car {
+    public String model;
+    public String color;
+    public String makeYear;
+    public String fuelType;
+
+    public Car() {
+
+    }
+
+    public Car(String model, String color, String makeYear, String fuelType) {
+        this.model = model;
+        this.makeYear = makeYear;
+        this.fuelType = fuelType;
+        this.color = color;
+    }
+
+    public Car(String model, String color, String fuelType) {
+        this.model = model;
+        this.color = color;
+        this.fuelType = fuelType;
+    }
+
+    public void startEngine(){
+        System.out.println("Silnik "+ this.model +" został uruchomiony");
+    }
+
+    public void stopEngine(){
+        System.out.println("Silnik został zwolnionyo");
+    }
+}
